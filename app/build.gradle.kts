4647d85f190215ed8f1bf4c199c97ec4d7d87652
Rados Kevin <rkevin99@gmail.com>
2024-09-25 13:39:55 +0200

    Project Created
    Created the android app, added dagger/hilt and ViewModel dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+    alias(libs.plugins.google.dagger.hilt)
+}
+
+android {
+    namespace = "hu.radosdev.szilagyiapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "hu.radosdev.szilagyiapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.google.dagger.hilt)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    kapt(libs.google.dagger.hilt.compiler)
+}
+
+kapt {
+    correctErrorTypes = true
+}
