5b3a53f02500fd39cb41edf76561cfe68d3d4aef
Rados Kevin <rkevin99@gmail.com>
2024-09-25 21:11:18 +0200

    Application Base
    App base class for Dagger/Hilt.

---
+++ app/src/main/java/hu/radosdev/szilagyiapp/data/hilt/SzilagyiApplication.kt
@@ -0,0 +1,9 @@
+package hu.radosdev.szilagyiapp.data.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SzilagyiApplication : Application() {
+
+}
