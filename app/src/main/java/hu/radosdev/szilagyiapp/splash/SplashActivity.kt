23264f79d83cdbf3980e73bd6120ce50fba0c216
Rados Kevin <rkevin99@gmail.com>
2024-09-30 16:34:45 +0200

    Splash Activity
    Splash Activity updated, ViewModel deleted because it is not required this time.

--- app/src/main/java/hu/radosdev/szilagyiapp/splash/SplashActivity.kt
+++ app/src/main/java/hu/radosdev/szilagyiapp/splash/SplashActivity.kt
@@ -4,21 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.AndroidEntryPoint
 import hu.radosdev.szilagyiapp.MainActivity
 import hu.radosdev.szilagyiapp.R
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
-
-    private lateinit var splashViewModel: SplashViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,24 +26,10 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        /*
-
-        splashViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
-
-        splashViewModel.isDataLoaded.observe(this, Observer { isLoaded ->
-            if(isLoaded){
-                // Amint megérkezik az adat, megyunk a main activity-ra
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            }
-        })
-
-        */
         Handler(Looper.getMainLooper()).postDelayed({
-            // Navigate to MainActivity after 2 seconds
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 10000)
+        }, 2000)
 
     }
 }
