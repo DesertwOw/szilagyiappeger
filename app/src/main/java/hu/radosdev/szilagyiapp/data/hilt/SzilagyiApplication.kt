1bdefdfb4d01421c800d9354c0be686099df3c6f
Rados Kevin <rkevin99@gmail.com>
2024-09-30 16:38:06 +0200

    Firebase refactor

--- app/src/main/java/hu/radosdev/szilagyiapp/data/hilt/SzilagyiApplication.kt
+++ app/src/main/java/hu/radosdev/szilagyiapp/data/hilt/SzilagyiApplication.kt
@@ -1,9 +1,13 @@
 package hu.radosdev.szilagyiapp.data.hilt
 
 import android.app.Application
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Provides
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Singleton
 
 @HiltAndroidApp
 class SzilagyiApplication : Application() {
+
 
 }
