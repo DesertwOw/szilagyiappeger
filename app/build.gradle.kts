a7fbe68b44864b8c6afab2c430c334ea570e2718
Rados Kevin <rkevin99@gmail.com>
2024-10-19 18:46:13 +0200

    Version upgrade
    Its working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,16 +40,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17 // or VERSION_11
+        targetCompatibility = JavaVersion.VERSION_17 // or VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
+    implementation(libs.kotlin.stdlib)
+    kapt(libs.kotlin.stdlib)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
