9f3db070df57d042025bf5ba1a9e3be7450cda1b
Kevin Rados <rkevin99@gmail.com>
2024-10-21 20:30:53 +0200

    Enable notifications related changes
    Switched the menu to hungarian language.

--- app/src/main/java/hu/radosdev/szilagyiapp/util/Constants.kt
+++ app/src/main/java/hu/radosdev/szilagyiapp/util/Constants.kt
@@ -4,20 +4,20 @@ class Constants {
     companion object{
 
         const val URL = "url"
-        const val CANCEL = "Cancel"
+        const val CANCEL = "Mégse"
         const val PACKAGE = "package"
         const val APP_PREFS = "app_prefs"
         const val ERROR_ARG = "error_message"
         const val CONTAINS_URL_STRING = "szilagyi"
-        const val GO_TO_SETTINGS = "Go to Settings"
+        const val GO_TO_SETTINGS = "Beállítások"
         const val FIRST_LAUNCH_FLAG = "is_first_launch"
-        const val PERMISSION_TITLE = "Enable Notifications"
+        const val PERMISSION_TITLE = "Értesítések engedélyezése"
         const val PREFERENCES_NAME = "szilagyi_preferences"
         const val BASE_URL =  "https://www.szilagyi-eger.hu/"
         const val DEFAULT_ERROR_MESSAGE = "Hoppá, valami hiba történt."
         const val URL_JSON_LOAD_ERROR = "Hiba a menü betöltése közben, nézz vissza később."
         const val NO_INTERNET_CONNECTION_DEFAULT_STRING = "Ellenőrizd az internetkapcsolatod."
-        const val UPDATE_MESSAGE = "To receive important updates, please enable notifications in the settings."
+        const val UPDATE_MESSAGE = "Ahhoz, hogy értesülj a legújjabb cikkekről, engedélyezd az értesítéseket."
 
 
         const val SPLASH_DELAY = 2000L
